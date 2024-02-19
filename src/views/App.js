75f29e80fd3bcfe5313a84fc17c2d0656baf2d25
lephuchung <lephuchung003@gmail.com>
2024-02-19 21:39:40 +0700

    video4

--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.scss';
+import MyCpnTest from './examples/MyCpnTest';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <MyCpnTest> </MyCpnTest>
       </header>
     </div>
   );
---
+++ src/views/examples/MyCpnTest.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class MyCpnTest extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    Hello from MyComponent Test
+                </p>
+            </div>
+        )
+    }
+}
+
+export default MyCpnTest;
