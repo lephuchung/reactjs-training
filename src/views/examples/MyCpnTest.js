3d2db621f6f1b7c3c5c70683e45c08497dba8ccf
lephuchung <lephuchung003@gmail.com>
2024-02-20 14:09:13 +0700

    video5

--- src/views/examples/MyCpnTest.js
+++ src/views/examples/MyCpnTest.js
@@ -1,13 +1,35 @@
 import React from "react";
 
 class MyCpnTest extends React.Component {
+
+    state = {
+        name: 'Le Phuc Hung',
+        uni: 'Hust'
+    }
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
     render() {
         return (
-            <div>
-                <p>
-                    Hello from MyComponent Test
-                </p>
-            </div>
+            <>
+                <div className="first">
+                    <input value={this.state.name} type="text"
+                        onChange={(event) => this.handleOnChangeName(event)} />
+                    <p>
+                        Hello from MyComponent Test,
+                        My name is {this.state.name}
+                    </p>
+                </div>
+                <div className="second">
+                    <p>
+                        I'm from {this.state['uni']}
+                    </p>
+                </div>
+            </>
         )
     }
 }
