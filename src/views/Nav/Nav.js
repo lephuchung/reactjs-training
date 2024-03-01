09801e9f60d99b911680d09b87c844c9672282c0
lephuchung <lephuchung003@gmail.com>
2024-03-01 23:51:35 +0700

    video23

--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -1,14 +1,27 @@
 import React from "react";
-import './Nav.scss'
+import './Nav.scss';
+import {
+    Link, NavLink
+} from 'react-router-dom';
 
 class Nav extends React.Component {
     render() {
         return (
             <>
+                {/* Sử dụng Link hoặc NavLink sẽ  giúp trang không load lại mỗi lần nhấn link */}
                 <div className="topnav">
-                    <a className="active" href="/">Home</a>
-                    <a href="/todo">Todos</a>
-                    <a href="/about">About</a>
+                    <NavLink to='/' activeClassName='active' exact={true}>
+                        Home
+                    </NavLink>
+                    <NavLink to='/todo' activeClassName='active' >
+                        Todos
+                    </NavLink>
+                    <NavLink to='/about' activeClassName='active' >
+                        About
+                    </NavLink>
+                    {/* <Link to='/'>Home</Link>
+                    <Link to='/todo'>Todos</Link>
+                    <Link to='/about'>About</Link> */}
                 </div>
             </>
         )
